9b24bce7ea62b480aaa41557945c8fc926d363ff
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-30 22:34:52 +0900

    It can be too early to use ES6 JUST to try Service Worker

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ Notification.requestPermission(function(status){
   console.log('Permission: ', status);
 });
 
-let startButton = document.getElementsByClassName('startButton')[0];
+var startButton = document.getElementsByClassName('startButton')[0];
 
 startButton.addEventListener('click', function(){
   // from http://www.html5rocks.com/ja/tutorials/service-worker/introduction/
