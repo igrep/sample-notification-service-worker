ea6ebfdb9517cf4bd472d0dec1efde060f7bedc5
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-30 22:18:06 +0900

    [WORKS on Chrome 44 on Linux] second sample

--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,11 @@
 function notify(){
   if (Notification.permission === 'granted'){
     console.log('Calling new Notification() from ServiceWorker... Doki doki!');
-    let n = new Notification('Yay! Successfully notified from a ServiceWorker!');
-    n.onshow = function () {
-      setTimeout(n.close, 5000);
-    }
+    self.registration.showNotification('Yay! Successfully notified from a ServiceWorker!');
   } else if (Notification.permission === 'default'){
     console.warn('Hay! The user has not granted nor denied yet! Reload the page!');
   } else {
     console.warn('OMG! The user has denied notification! Give up!');
   }
 }
-setInterval(notify, 1000);
+setInterval(notify, 3000);
